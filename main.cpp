590452b61809278b8d47112f19799277c226ce6f
vivienwang-wei <76478892+vivienwang-wei@users.noreply.github.com>
2023-04-11 16:04:21 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include"Data.h"
+#include"definition.h"
+#include"model.h"
+
+// Command line arguments are required.
+
+int main(int argc, char** argv) {
+	try {
+		ScenarioSet SS;
+		SS.set_scenarios("imput\\scenario.txt");
+		SS.print();
+
+		TypeSet TS;
+		TS.set_cost("imput\\typeCost.txt");
+		TS.set_feasible("imput\\typeFeasible.txt");
+		TS.print();
+
+		Parameter parameter;
+		parameter.TypeNumber = TS.category;
+		parameter.MethodNumber = 3;
+		parameter.totalNo = {6612,2628,1608,1152};
+		parameter.alpha = 0.05;
+		parameter.beta = 0.05;
+		parameter.set_noPerSet(SS);
+		parameter.set_shipNumber();
+		parameter.set_cost(TS);
+		parameter.set_methodFeasible(TS);
+		parameter.print();
+		
+		solve(6, parameter);
+	}
+	catch (const exception& exc) {
+		printErrorAndExit("main", exc);
+	}
+	return 0;
+}
