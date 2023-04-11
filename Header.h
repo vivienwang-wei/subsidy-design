1f53d73cfae592ebd9c507213be80f2c32672528
vivienwang-wei <76478892+vivienwang-wei@users.noreply.github.com>
2023-04-11 16:07:33 +0800

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,77 @@
+#pragma once
+
+// A header file 
+
+#include<algorithm>
+#include<bitset>
+#include<ctime>
+#include<fstream>
+#include<functional>
+#include<ilcplex/ilocplex.h>
+#include<io.h>
+#include<iostream>
+#include<iomanip>
+#include<iterator>
+#include<list>
+#include<map>
+#include<mutex>
+#include<numeric>
+#include<queue>
+#include<random>
+#include<sstream>
+#include<string>
+#include<set>
+#include<thread>
+#include<tuple>
+#include<unordered_set>
+#include<unordered_map>
+#include<vector>
+#include<chrono>
+#include<cmath>
+
+
+using std::istream;
+using std::ostream;
+using std::cout;
+using std::ofstream;
+using std::cerr;
+using std::ifstream;
+using std::stringstream;
+using std::ostringstream;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::exception;
+using std::thread;
+using std::mutex;
+using std::lock_guard;
+using std::default_random_engine;
+using std::uniform_real_distribution;
+using std::uniform_int_distribution;
+using std::normal_distribution;
+using std::string;
+using std::vector;
+using std::list;
+using std::set;
+using std::multiset;
+using std::unordered_set;
+using std::map;
+using std::multimap;
+using std::unordered_map;
+using std::queue;
+using std::priority_queue;
+using std::tuple;
+using std::pair;
+using std::make_pair;
+using std::make_tuple;
+using std::get;
+using std::bitset;
+using std::ref;
+using std::min;
+using std::max;
+using std::greater;
+using std::sort;
+using std::prev;
+using std::next;
+using std::random_shuffle;
+
